933654cc52048ef716b1dcb0816cdece8009feda
Kathy Wang <k.wang6898@gmail.com>
2014-12-10 23:08:56 -0500

    added body for ActionListener

---
+++ 05GUICtoF/TempConvert.java
@@ -0,0 +1,49 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class TempConvert extends JFrame implements ActionListener{
+    private Container pane;
+
+    private JButton toFr;
+    private JButton toCl;
+    private JTextField num;
+    private String temp;
+
+    public TempConvert(){
+	this.setTitle("Temperature Converter 2k14 (ver 1.0.0)");
+	this.setSize(600,200);
+	this.setLocation(100,100);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+	pane = this.getContentPane();
+	pane.setLayout(new FlowLayout());
+
+	toFr = new JButton("Convert to Farenheit");
+	toCl = new JButton("Convert to Celsius");
+	num = new JTextField(6);
+	toFr.addActionListener(this); 
+	toCl.addActionListener(this);
+	toFr.setActionCommand("CtF");
+	toCl.setActionCommand("FtC");
+	pane.add(num);
+	pane.add(toFr);
+	pane.add(toCl);
+
+
+    }
+
+    public void actionPerformed(ActionEvent e){
+	int intTemp = Integer.parseInt(num.getText());
+	if (e.getActionCommand.equals("CtF")){
+	    System.out.println(intTemp * 9/5 + 32); 
+	}
+	if (e.getActionCommand.equals("FtC")){
+	    System.out.println((intTemp - 32) * 5/9);
+	}
+    }
+
+    public static void main(String[]args){
+	TempConvert a = new TempConvert();
+	a.setVisible(true);
+    }
+}
