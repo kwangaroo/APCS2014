0dbb4a759ce9535a0eff99a8733785ed54954e1a
Kathy Wang <kwangaroo@users.noreply.github.com>
2014-12-16 13:18:04 -0500

    Create Sorts.java

---
+++ 06Sorts/Sorts.java
@@ -0,0 +1,22 @@
+public class Sorts{
+    private int[]test;
+
+    public Sorts(int[]a){
+    	test = a;
+    }
+ 
+    public void insertionSort(){
+    	int sv = 0;
+    	int sav = 0;
+    	for(int i=1;i<size();i++){
+	      sv = initial[i];
+	      for(int j=i;j>0;j--){
+	    	if(initial[j].compareTo(sv) < 0){
+		      initial[j] = sav;  
+		      set(j, sv);
+		      add(sav);
+		      j = 0;
+	  	  }
+	    }
+	  } 
+}
