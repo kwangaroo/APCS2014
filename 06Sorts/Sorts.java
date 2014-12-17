8afc74b9c1a2a3fe2cf2bbbdc9395f345fc3aef4
Kathy Wang <k.wang6898@gmail.com>
2014-12-16 22:23:07 -0500

    added bubbleSort and vamped up old sorts.

--- 06Sorts/Sorts.java
+++ 06Sorts/Sorts.java
@@ -1,24 +1,51 @@
 public class Sorts{
-    private int[]test;
+    private int[]list;
     
     public Sorts(int[]a){
     	test = a;
     }
     
-    public void insertionSort(){
-    	int sv = 0;
-    	int sav = 0;
-    	for(int i=1;i<size();i++){
-	    sv = initial[i];
-	    for(int j=i;j>0;j--){
-	    	if(initial[j].compareTo(sv) < 0){
-		    initial[j] = sav;  
-		    set(j, sv);
-		    add(sav);
-		    j = 0;
+    public static void insertionSort() {
+        for (int i = 1; i < list.length; i++) {
+            int next = list[i];
+	    int j = i;
+            while (j > 0 && list[j - 1] > next) {
+                list[j] = list[j - 1];
+                j--;
+            }
+	    list[j] = next;
+        }
+    }
+	
+    private static void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+    
+    public static void selectionSort() {
+        for (int i = 0; i < list.length - 1; i++) {
+	    int min = i;
+            for (int j = i + 1; j < list.length; j++) {
+                if (list[j] < list[min]) {
+                    min = j;
+                }
+            }
+            swap(list, minPos, i);
+        }
+    }
+    
+    public static void bubbleSort(){
+        int temp;
+        for (int i=0;i<list.length;i++) {
+	    for(int j = 0; j < list.length; j++){
+		if(list[i] > list[j + 1]) {
+		    temp = list[j+1];
+		    list[j+1] = list[i];
+		    list[i] = temp;
 		}
 	    }
-	} 
+        }
     }
     
 }
